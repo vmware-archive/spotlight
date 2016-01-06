de495ff6d6df6d9887aceab915ee33a0b8379279
Michael Cheng + Rahul Rajeev <sg+pair+mc+rr@neo.com>
2016-01-06 17:57:37 +0800

    Fix broken test.
    [no-pt-story]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,13 +16,6 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
-  end
-end
-
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest

2e96c14f53a597df3039219da9db09ea4fd8f67e
Rahul Rajeev <rahul@neo.com>
2016-01-11 17:40:48 +0800

    Migration to add UUID to widgets
    [##111398356]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160106084913) do
+ActiveRecord::Schema.define(version: 20160111091203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,8 +30,10 @@ ActiveRecord::Schema.define(version: 20160106084913) do
     t.integer  "dashboard_id",                      null: false
     t.datetime "created_at",                        null: false
     t.datetime "updated_at",                        null: false
+    t.string   "uuid",                              null: false
   end
 
   add_index "widgets", ["dashboard_id"], name: "index_widgets_on_dashboard_id", using: :btree
+  add_index "widgets", ["uuid"], name: "index_widgets_on_uuid", unique: true, using: :btree
 
 end

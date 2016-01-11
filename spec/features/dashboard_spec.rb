9d98bca23a8e6f4624b5a41aaca0acc8d2f456a2
Rahul Rajeev <rahul@neo.com>
2016-01-11 17:08:25 +0800

    Feature test for travis ci widget
    [#111128318]

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 describe "the dashboard widget creation", :type => :feature do
+  let(:widget_title) {'new widget'}
+
   before :each do
     Dashboard.create(title: 'Default Dashboard')
   end
@@ -9,10 +11,10 @@ describe "the dashboard widget creation", :type => :feature do
     visit '/'
     click_link 'New Widget'
     expect(page).to have_css '#qa-new-widget-form'
-    fill_in 'Title', :with => 'neo/spotlight'
+    fill_in 'Title', :with => widget_title
     click_button 'Submit'
     visit '/'
-    expect(page).to have_content 'neo/spotlight'
+    expect(page).to have_content widget_title
     expect(page).to have_selector('.widget', count: 1)
   end
 end

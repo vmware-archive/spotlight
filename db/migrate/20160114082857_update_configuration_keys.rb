2ce6115fe177e5872c4116c2b6e59ae09eb3ec8f
Michael Cheng NEOmichael.cheng@neo.com <sg+mcn@neo.com>
2016-01-14 17:05:59 +0800

    Fix typo.
    [no-pt-story]

--- db/migrate/20160114082857_update_configuration_keys.rb
+++ db/migrate/20160114082857_update_configuration_keys.rb
@@ -5,7 +5,7 @@ class UpdateConfigurationKeys < ActiveRecord::Migration
         current_config = JSON.parse(widget.configuration).with_indifferent_access
         if current_config[:travis_auth_key]
           travis_uri = URI.parse(current_config[:travis_url])
-          server_url = travis_uri.scheme + '//' + travis_uri.host
+          server_url = travis_uri.scheme + '://' + travis_uri.host
 
           widget.update(server_type: 'travis_ci',
                         server_url: server_url,

f91feeb11635af5421bcdfb43d07fa24c60d40c6
Rahul Rajeev <rahul@neo.com>
2016-01-15 15:58:04 +0800

    migration to add size details to widgets
    [#111479218]

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe WidgetsController, type: :controller do
     end
 
     it "assigns a new widget as @widget" do
-      get :new, {category: 'ci_widget'}, valid_session
+      get :new, {category: 'ci_widget'}
       expect(assigns(:widget)).to be_a_new(Widget)
       expect(assigns(:widget).category).to be_a Category::CiWidget
     end
@@ -25,26 +25,36 @@ RSpec.describe WidgetsController, type: :controller do
         server_type: 'travis_ci',
         server_url: 'https://api.travis-ci.com',
         auth_key: 'access_token',
-        project_name: 'neo/spotlight'
+        project_name: 'neo/spotlight',
+        height: '200',
+        width: '200'
       }
     end
 
     context "with valid params" do
       it "creates a new Widget" do
         expect {
-          post :create, {:widget => valid_attributes}, valid_session
+          post :create, {:widget => valid_attributes}
         }.to change(Widget, :count).by(1)
       end
 
       it "assigns a newly created widget as @widget" do
-        post :create, {:widget => valid_attributes}, valid_session
+        post :create, {:widget => valid_attributes}
         expect(assigns(:widget)).to be_a(Widget)
         expect(assigns(:widget)).to be_persisted
       end
 
       it "redirects to the dashboard" do
-        post :create, {:widget => valid_attributes}, valid_session
+        post :create, {:widget => valid_attributes}
         expect(response).to redirect_to(dashboards_path)
+      end
+
+      it "saves the widget title and size" do
+        post :create, {:widget => valid_attributes}
+        new_widget = Widget.last
+        expect(new_widget.title).to eq 'test'
+        expect(new_widget.height).to eq 200
+        expect(new_widget.width).to eq 200
       end
     end
 
@@ -53,12 +63,12 @@ RSpec.describe WidgetsController, type: :controller do
         let(:invalid_attributes) { {title: nil, category: 'ci_widget'} }
 
         it "assigns a newly created but unsaved widget as @widget" do
-          post :create, {:widget => invalid_attributes}, valid_session
+          post :create, {:widget => invalid_attributes}
           expect(assigns(:widget)).to be_a_new(Widget)
         end
 
         it "re-renders the 'new' template" do
-          post :create, {:widget => invalid_attributes}, valid_session
+          post :create, {:widget => invalid_attributes}
           expect(response).to render_template("new")
         end
       end
@@ -67,12 +77,12 @@ RSpec.describe WidgetsController, type: :controller do
         let(:invalid_attributes) { {title: Faker::Lorem.characters(61), category: 'ci_widget' } }
 
         it "assigns a newly created but unsaved widget as @widget" do
-          post :create, {:widget => invalid_attributes}, valid_session
+          post :create, {:widget => invalid_attributes}
           expect(assigns(:widget)).to be_a_new(Widget)
         end
 
         it "re-renders the 'new' template" do
-          post :create, {:widget => invalid_attributes}, valid_session
+          post :create, {:widget => invalid_attributes}
           expect(response).to render_template("new")
         end
       end

81cb8449c179cbc2d91eb679667d5017d463efa9
Michael Cheng <sg+mcn@neo.com>
2016-01-15 16:09:47 +0800

    Test for TravisCiService class.
    [#111478910]

---
+++ spec/services/travis_ci_service_spec.rb
@@ -0,0 +1,112 @@
+require 'rails_helper'
+
+RSpec.describe TravisCiService do
+  let(:server_url) { 'https://api.travis-ci.com' }
+  let(:auth_key) { 'auth_key' }
+  let(:project_name) { 'neo/spotlight' }
+  let(:opts) do
+    {
+      server_url: server_url,
+      auth_key: auth_key,
+      project_name: project_name
+    }
+  end
+
+  subject{ TravisCiService.new(opts) }
+
+  describe '#initialize' do
+    it 'sets default values' do
+      expect(subject.server_url).to eq server_url
+      expect(subject.auth_key).to eq auth_key
+      expect(subject.project_name).to eq project_name
+    end
+  end
+
+  describe '.for_widget' do
+    let(:dashboard) { FactoryGirl.create :dashboard }
+    let(:widget){
+      FactoryGirl.create :widget,
+                  dashboard: dashboard,
+                  category: 'ci_widget',
+                  server_type: 'travis_ci',
+                  server_url: server_url,
+                  auth_key: auth_key,
+                  project_name: project_name
+    }
+
+    subject{ TravisCiService.for_widget(widget) }
+
+    it 'sets default values' do
+      expect(subject.server_url).to eq server_url
+      expect(subject.auth_key).to eq auth_key
+      expect(subject.project_name).to eq project_name
+    end
+  end
+
+  describe '#repo_info' do
+    it 'makes request to repo' do
+      mock_request = stub_request(:get, "#{server_url}/repos/#{project_name}").
+          with(headers: { 'Accept' => 'application/vnd.travis-ci.2+json',
+                          'Authorization' => 'Token "' + auth_key + '"' }).
+          to_return(body:    '{"foo":"bar"}',
+                    headers: {'Content-Type' => 'application/json'})
+
+      result = subject.repo_info
+
+      expect(mock_request).to have_been_made
+      expect(result.body["foo"]).to eq "bar"
+    end
+  end
+
+  describe '#build_info' do
+    let(:build_id) { 12345 }
+
+    it 'makes request to repo' do
+      mock_request = stub_request(:get, "#{server_url}/repos/#{project_name}/builds/#{build_id}").
+          with(headers: { 'Accept' => 'application/vnd.travis-ci.2+json',
+                          'Authorization' => 'Token "' + auth_key + '"' }).
+          to_return(body:    '{"foo":"bar"}',
+                    headers: {'Content-Type' => 'application/json'})
+
+      result = subject.build_info(build_id)
+
+      expect(mock_request).to have_been_made
+      expect(result.body["foo"]).to eq "bar"
+    end
+  end
+
+  describe '#last_build_info' do
+    let(:build_id) { 12345 }
+    let(:last_build_status) { 'success' }
+    let(:last_build_time) { '2016-01-15 11:00:00 +08:00' }
+    let(:last_committer) { 'Rahul Rajeev' }
+
+    it 'makes request to repo' do
+      repo_request = stub_request(:get, "#{server_url}/repos/#{project_name}").
+          with(headers: { 'Accept' => 'application/vnd.travis-ci.2+json',
+                          'Authorization' => 'Token "' + auth_key + '"' }).
+          to_return(body: {"repo" => {"last_build_id" => build_id}}.to_json,
+                    headers: {'Content-Type' => 'application/json'})
+
+      build_response_body = {"build" => { "state" => last_build_status,
+                                          "finished_at" => last_build_time },
+                             "commit" => { "author_name" => last_committer }}.to_json
+      build_request = stub_request(:get, "#{server_url}/repos/#{project_name}/builds/#{build_id}").
+          with(headers: { 'Accept' => 'application/vnd.travis-ci.2+json',
+                          'Authorization' => 'Token "' + auth_key + '"' }).
+          to_return(body: build_response_body,
+                    headers: {'Content-Type' => 'application/json'})
+
+      result = subject.last_build_info
+
+      expect(repo_request).to have_been_made
+      expect(build_request).to have_been_made
+      expect(result.keys).to include :repo_name, :last_build_status, :last_committer, :last_build_time
+      expect(result[:repo_name]).to eq project_name
+      expect(result[:last_build_time]).to eq last_build_time
+      expect(result[:last_build_status]).to eq last_build_status
+      expect(result[:last_committer]).to eq last_committer
+    end
+
+  end
+end

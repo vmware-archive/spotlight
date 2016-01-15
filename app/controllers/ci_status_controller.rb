19faf322da10acb03ab4ca4a9722f4dddb492a66
Michael Cheng <sg+mcn@neo.com>
2016-01-15 17:48:51 +0800

    Use meta programming to get to the Service class.
    [#111478910]

--- app/controllers/ci_status_controller.rb
+++ app/controllers/ci_status_controller.rb
@@ -1,11 +1,6 @@
 class CiStatusController < ApplicationController
   def show
-    case widget.server_type
-    when 'travis_ci'
-      service_class = TravisCiService
-    when 'jenkins_ci'
-      service_class = JenkinsCiService
-    end
+    service_class = (widget.server_type + '_service').camelize.constantize
 
     @ci_status = service_class.for_widget(widget).last_build_info
   end

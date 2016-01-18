7fb96465915c0bd869f4b3cdabe23b83b53831e3
Michael Cheng + Rahul Rajeev <sg+pair+mc+rr@neo.com>
2016-01-18 15:29:31 +0800

    Install React-Rails
    [#111481180]

--- config/application.rb
+++ config/application.rb
@@ -32,5 +32,8 @@ module Spotlight
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.autoload_paths += %W(#{config.root}/app/enums)
+
+    config.react.variant      = :production
+    config.react.addons       = true
   end
 end

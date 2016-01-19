25f577173964d323e1c02626127337263c1060ca
Divya Bhargov <divya.bhargov@gmail.com>
2016-01-19 09:29:01 +0800

    Remove the explicit failing spec that was introduced to test failed build

--- spec/models/dashboard_spec.rb
+++ spec/models/dashboard_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe Dashboard, type: :model do
   describe 'associations' do
     it { should have_many(:widgets) }
   end
-
-  it 'fails - explicitly added for Gabe to be able to accept story #111128318' do
-    fail
-  end
 end

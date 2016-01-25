eb91bd41c99c87e905c044f6b3469d2a70d5be23
Divya Bhargov + Rahul Rajeev <sg+pair+db+rr@neo.com>
2016-01-25 10:36:49 +0800

    More test coverage for ciWidget react component
    [no-pt-story]

--- spec/javascripts/components/ci_widget_spec.jsx
+++ spec/javascripts/components/ci_widget_spec.jsx
@@ -1,16 +1,35 @@
 describe('CiWidget', function () {
-  it('renders the title', function () {
-    var testTitle = "Concierge";
-    var ciWidget = window.TestUtils.renderIntoDocument(
-      <CiWidget title={testTitle} />
-    );
+  var ciWidget;
+  var testTitle = "Concierge";
+  var testUuid = "123456789";
+  var testPath = '/widget_path'
+  var widgetProps = {
+    uuid: testUuid,
+    title: testTitle,
+    widget_path: testPath
+  }
 
+  beforeEach(function() {
+    ciWidget = window.TestUtils.renderIntoDocument(
+      <CiWidget {...widgetProps}/>
+    );
+  });
+
+  it('renders the title', function () {
     var titleNode = window.TestUtils.findRenderedDOMComponentWithTag(
       ciWidget,
       'h4'
     )
-
     expect(titleNode.textContent).toEqual(testTitle);
   });
 
+  it ('widget contains the uuid information', function() {
+    expect(ciWidget.getDOMNode().dataset.uuid).toEqual(testUuid);
+  });
+
+
+  it('renders the delete button', function () {
+    var deleteLink = window.TestUtils.findRenderedDOMComponentWithClass(ciWidget, 'delete')
+    expect(deleteLink.href).toContain(testPath);
+  });
 });

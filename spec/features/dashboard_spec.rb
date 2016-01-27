11a20892b4efb32453e70e76c7854942176b5169
Gabe Hollombe + Rahul Rajeev <sg+pair+gh+rr@neo.com>
2016-01-27 17:10:12 +0800

    Write feature test for repositioning and saving
    [#112396813]

--- spec/features/dashboard_spec.rb
+++ spec/features/dashboard_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
+def get_offset(node)
+  node[:style]
+    .match(/translate\((.*)px, (.*)px\)/)[1..2]
+    .map(&:to_i)
+end
+
 describe "the dashboard widget creation", :type => :feature do
   let(:widget_title) {'new widget'}
 
@@ -31,7 +37,7 @@ describe "the dashboard widget creation", :type => :feature do
 
   context 'has existing widget' do
     before do
-      FactoryGirl.create(:widget, dashboard: Dashboard.first, height: 2)
+      FactoryGirl.create(:widget, dashboard: Dashboard.first, height: 2, width: 2, position_x: 0, position_y: 0)
     end
 
     it "must be able to delete a widget", js: true do
@@ -46,11 +52,13 @@ describe "the dashboard widget creation", :type => :feature do
     end
 
     it 'renders the widget in the correct position', js: true do
+      vertical_offset = 2
+      horizontal_offset = 1
+
       new_widget = FactoryGirl.create(:widget, dashboard: Dashboard.first,
-        position_x: 2, position_y: 1,
-        height: 4, width: 5 )
-      row_height = 100;
-      delta = 10
+        position_x: horizontal_offset,
+        position_y: vertical_offset,
+        height: 4, width: 4 )
 
       visit '/'
 
@@ -58,13 +66,37 @@ describe "the dashboard widget creation", :type => :feature do
       widget_node = ci_widget_node.find(:xpath, '..')
       #widget is the parent div of the ci_widget.
 
-      position_x, position_y = widget_node[:style]
-                                  .match(/translate\((.*)px, (.*)px\)/)[1..2]
-                                  .map(&:to_i)
+      position_x, position_y = get_offset(widget_node)
       window_width = page.current_window.size[0]
 
-      expect(position_x).to be_within(delta).of(window_width/12 * 2)
-      expect(position_y).to be_within(delta).of(row_height * 2)
+      row_height = 100
+      column_count = 12
+      delta = 10
+      expect(position_x).to be_within(delta).of(window_width/column_count * horizontal_offset)
+      expect(position_y).to be_within(delta).of(row_height * vertical_offset)
+    end
+
+    it 'saves the layout changes and redirects to dashboards path', js:true do
+      visit '/'
+      click_link 'edit'
+      sleep(1)
+
+      widget = page.first('.widget')
+      original_offsets = get_offset(widget)
+
+      target = page.first('.save-button')
+      widget.drag_to(target)
+
+      after_drag_offsets = get_offset(widget)
+      expect(original_offsets).to_not eq after_drag_offsets
+
+      click_link 'save'
+      expect(page.current_path).to eq '/dashboards'
+
+      sleep(1)
+      after_save_offsets = get_offset page.first('.widget')
+      expect(after_save_offsets).to eq after_drag_offsets
+
     end
   end
 end

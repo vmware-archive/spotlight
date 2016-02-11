0bc928e1a6201ce341ea15ac6e73e76b4157fff9
Michael Cheng <sg+mcn@neo.com>
2016-02-11 15:08:39 +0800

    Refactor Google login controller.
    [#113142375]

--- app/controllers/api/google_controller.rb
+++ app/controllers/api/google_controller.rb
@@ -4,25 +4,13 @@ class Api::GoogleController < ApplicationController
   def login
     session[:return_url] = params[:return_url]
 
-    client = Signet::OAuth2::Client.new({
-                                            client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
-                                            client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
-                                            authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
-                                            scope: API_SCOPE,
-                                            redirect_uri: url_for(action: :callback)
-                                        })
+    client = Signet::OAuth2::Client.new( oauth2_config.merge({scope: API_SCOPE}) )
 
     redirect_to client.authorization_uri.to_s
   end
 
   def callback
-    client = Signet::OAuth2::Client.new({
-                                            client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
-                                            client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
-                                            token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
-                                            redirect_uri: url_for(action: :callback),
-                                            code: params[:code]
-                                        })
+    client = Signet::OAuth2::Client.new( oauth2_config.merge({code: params[:code]}) )
 
     response = client.fetch_access_token!
 
@@ -30,5 +18,16 @@ class Api::GoogleController < ApplicationController
     session[:refresh_token] = response['refresh_token']
 
     redirect_to session[:return_url]
+  end
+
+  private
+
+  def oauth2_config
+    {
+      client_id: ENV.fetch('GOOGLE_API_CLIENT_ID'),
+      client_secret: ENV.fetch('GOOGLE_API_CLIENT_SECRET'),
+      token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+      redirect_uri: url_for(action: :callback),
+    }
   end
 end

681e4d8b33e7326d5a05293656df46424c131ebf
Michael Cheng <sg+mcn@neo.com>
2016-02-12 10:09:39 +0800

    Ensure that test won't break when the Google environment
    [#113142375]

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
+
+ENV['GOOGLE_API_CLIENT_ID'] = 'fake-api-id'
+ENV['GOOGLE_API_CLIENT_SECRET'] = 'fake-api-secret'
+
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'

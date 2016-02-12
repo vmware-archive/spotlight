d25aa076057e3d865d436ced2becb45be76abd0c
Divya Bhargov <divya.bhargov@neo.com>
2016-02-12 11:05:30 +0800

    Fixing build

--- spec/controllers/widgets_controller_spec.rb
+++ spec/controllers/widgets_controller_spec.rb
@@ -41,7 +41,7 @@ RSpec.describe WidgetsController, type: :controller do
 
       it "redirects to the dashboard" do
         post :create, {:widget => valid_attributes}
-        expect(response).to redirect_to(ENV['WEB_HOST'])
+        expect(response).to redirect_to(dashboards_path)
       end
 
       it "saves the widget title and size" do

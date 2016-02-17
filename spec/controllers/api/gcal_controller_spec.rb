38b122a7939318a0157a83751413218bce272110
Michael Cheng <sg+mcn@neo.com>
2016-02-17 11:16:17 +0800

    Fix failing test due to refresh token trying to make an actual call.
    [no-pt-story]

--- spec/controllers/api/gcal_controller_spec.rb
+++ spec/controllers/api/gcal_controller_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe Api::GcalController, type: :controller do
 
   describe 'GET #show' do
     let(:events) { double }
+    let(:authorization) { double }
 
     before do
+      allow(authorization).to receive(:refresh!).and_return(true)
+      expect_any_instance_of(GoogleAuthService).to receive(:client).and_return(authorization)
       expect_any_instance_of(GoogleCalendarService).to receive(:list_events).with(widget.calendar_id).and_return(events)
 
       get :show, id: widget.uuid, format: :json

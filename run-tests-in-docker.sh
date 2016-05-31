38fb428ee0d6af6eff1024f820c72f133962f7fd
James Rivett-Carnac and Michael Cheng <pair+jrivettcarnac+mcheng@pivotal.io>
2016-05-31 17:32:58 +0800

    vender the gems, create the concourse ci stuff

---
+++ run-tests-in-docker.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cd $(dirname $0)
+set -e
+export RAILS_ENV=test
+export TZ=Asia/Singapore
+cp -n env.sample .env
+cp -n config/database-local.yml config/database.yml
+
+####
+
+function run-rb() {
+    /etc/init.d/postgresql start
+    rake db:setup
+
+    echo -- Ruby tests
+    xvfb-run rspec $@
+}
+
+####
+
+case "$1" in
+    all)
+        echo "-- Running all tests"
+        run-rb
+        ;;
+    rb)
+        run-rb
+        ;;
+    *)
+        echo "No type specified. Please specify one of: all, js, rb, or integration" >&2
+        exit 1
+        ;;
+esac

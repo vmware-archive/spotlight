28bc47b2bf8ed6a454e69fafe603e610cef415ca
Michael Cheng <pair+mcheng@pivotal.io>
2016-07-08 14:56:23 +0800

    Change name of Gem cache folder

--- run-test-in-concourse.sh
+++ run-test-in-concourse.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 WORK_DIR=`pwd`
+GEMS_FOLDER=$WORK_DIR/gem-cache
 
-mkdir -p $WORK_DIR/gems
+mkdir -p $GEMS_FOLDER
 
 cd spotlight-git
 cp config/database-docker.yml config/database.yml
 
 gem install bundler
-bundle install --path $WORK_DIR/gems
+bundle install --path $GEMS_FOLDER
 
 RAILS_ENV=test bundle exec rake db:drop db:create db:migrate
 bundle exec rake spec

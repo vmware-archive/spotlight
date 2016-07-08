fc07ac012973363ad37ff003d142a64426eb185f
Michael Cheng <pair+mcheng@pivotal.io>
2016-07-08 13:56:17 +0800

    Configure the use of a path

--- run-test-in-concourse.sh
+++ run-test-in-concourse.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+WORK_DIR=`pwd`
+
+mkdir -p $WORK_DIR/gems
+
 cd spotlight-git
 cp config/database-docker.yml config/database.yml
 
 gem install bundler
-bundle install --local
+bundle install --path $WORK_DIR/gems
 
 RAILS_ENV=test bundle exec rake db:drop db:create db:migrate
 bundle exec rake spec

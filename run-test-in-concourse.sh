523be2369f76301b054b0b1bbe7a92f2ca03770d
Michael Cheng <pair+mcheng@pivotal.io>
2016-07-11 11:12:53 +0800

    Use an archive file instead.

--- run-test-in-concourse.sh
+++ run-test-in-concourse.sh
@@ -2,9 +2,11 @@
 
 WORK_DIR=`pwd`
 GEMS_FOLDER=$WORK_DIR/bundle
+ARCHIVES_FOLDER=$WORK_DIR/archives
 
+# Recover cache folder
 mkdir -p $GEMS_FOLDER
-
+mkdir -p $ARCHIVES_FOLDER
 
 GEMS_CACHE=$WORK_DIR/build-cache
 OLD_BUNDLE_HASH=`ls $GEMS_CACHE`
@@ -14,6 +16,12 @@ then
   mv $GEMS_CACHE/$OLD_BUNDLE_HASH/bundle/ruby $GEMS_FOLDER/ruby
 fi
 
+if [ -d $GEMS_CACHE/$OLD_BUNDLE_HASH/archive/ruby.tar.gz ]
+then
+  tar -xzvf $GEMS_CACHE/$OLD_BUNDLE_HASH/archive/ruby.tar.gz -C $GEMS_FOLDER/
+fi
+
+# Run tests
 cd $WORK_DIR/spotlight-git
 cp config/database-docker.yml config/database.yml
 
@@ -22,5 +30,8 @@ bundle install --path $GEMS_FOLDER
 
 RAILS_ENV=test bundle exec rake db:drop db:create db:migrate
 bundle exec rake spec
+
+# Tarball the Rubies / Gems
+tar -cvzf $ARCHIVES_FOLDER/ruby.tar.gz $GEMS_FOLDER/ruby
 
 exit 0
